9d71be36ac87dd789231a4885437e8046f06f8b8
Topias-R <55441824+Topias-R@users.noreply.github.com>
2022-11-14 10:21:13 +0200

    Add responsive styles

--- seats/script.js
+++ seats/script.js
@@ -24,3 +24,22 @@ function closeTrailer() {
   document.getElementById("button1").style.display = "block";
   document.getElementById("buttonClose1").style.display = "none";
 }
+
+let clickmäärä = 0;
+function ShowMenu() {
+  if (window.screen.width > 426) {
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("header").style.display = "flex";
+  }
+
+  if (window.screen.width <= 426) {
+    clickmäärä++;
+    if (clickmäärä % 2 != 0) {
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("header").style.display = "block";
+      document.getElementById("header").style.textAlign = "center";
+    } else {
+      document.getElementById("menu").style.display = "none";
+    }
+  }
+}
