713e702b18e9fd496ac124eeaa27cf6e210fea69
HelenMayer <you@example.com>
2022-11-18 12:02:46 +0200

    add headerin script

--- shows/script-all-movies.js
+++ shows/script-all-movies.js
@@ -104,3 +104,27 @@ function CloseSarja() {
     document.getElementsByClassName("sarjat")[0].style.opacity="1"; 
 }
 
+
+let clickmäärä=0;
+function ShowMenu() {
+    if (window.screen.width>426) {
+        document.getElementById("menu").style.display="flex";
+        document.getElementById("header").style.display="flex";    
+    }
+   
+    if (window.screen.width<=426) {
+        clickmäärä++;
+        if (clickmäärä%2!=0) {
+            document.getElementById("menu").style.display="block";
+            document.getElementById("header").style.display="block";
+            document.getElementById("header").style.textAlign="center";
+        }
+        else{
+            document.getElementById("menu").style.display="none";
+        }
+    
+    }
+
+}
+
+
