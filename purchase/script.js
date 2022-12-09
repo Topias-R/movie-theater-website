26f91a91f2cc9356011ce3a54d3024ac335a6bc2
Topias-R <55441824+Topias-R@users.noreply.github.com>
2022-12-09 10:22:52 +0200

    Harmonize purchase page

--- purchase/script.js
+++ purchase/script.js
@@ -5,7 +5,8 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 const movie = movies[params.movie];
 
 document.getElementById("movie-title").textContent = movie.title;
-document.getElementById("movie-time").textContent = movie.time;
+document.getElementById("movie-time").textContent =
+  getNextAvailableShow(movie.time) + " • klo " + movie.time;
 document.getElementById("price").textContent =
   params.seats.split(";").length * 15 + "€";
 
@@ -16,3 +17,14 @@ const chosenSeats = params.seats.split(";").map((seat) => {
 });
 
 document.querySelector(".seats-list").replaceChildren(...chosenSeats);
+
+function getNextAvailableShow(time) {
+  const d = new Date();
+  const c = new Date();
+  c.setHours(time.split(":")[0] - 1, 0, 0, 0);
+  if (d.getTime() < c.getTime()) return d.toLocaleDateString();
+  else {
+    d.setDate(d.getDate() + 1);
+    return d.toLocaleDateString();
+  }
+}
